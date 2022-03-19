1bb05538311ec5e83d4696fcda3290e74d7af1e6
20shiy <shiying6120@gmail.com>
2022-03-19 00:47:22 -0400

    twitter app part 2

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -1,30 +1,32 @@
 package com.codepath.apps.restclienttemplate.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 import org.json.JSONObject
 import java.util.*
 import kotlin.collections.ArrayList
 
-class Tweet {
-    var tweetBody: String = ""
-    var createdAt: String = ""
-    var user: User? = null
+@Parcelize
+class Tweet(var tweetBody: String = "", var createdAt: String = "", var user: User ?= null) :
+    Parcelable {
 
-    companion object {
-        fun fromJson(jsonObject: JSONObject): Tweet {
-            val tweet = Tweet()
-            tweet.tweetBody = jsonObject.getString("text")
-            tweet.createdAt = jsonObject.getString("created_at")
-            tweet.user = User.fromJson(jsonObject.getJSONObject("user"))
-            return tweet
-        }
 
-        fun fromJsonArray(jsonArray: JSONArray): List<Tweet> {
-            val tweets = ArrayList<Tweet>()
-            for (i in 0 until jsonArray.length()) {
-                tweets.add(fromJson(jsonArray.getJSONObject(i)))
+        companion object {
+            fun fromJson(jsonObject: JSONObject): Tweet {
+                val tweet = Tweet()
+                tweet.tweetBody = jsonObject.getString("text")
+                tweet.createdAt = jsonObject.getString("created_at")
+                tweet.user = User.fromJson(jsonObject.getJSONObject("user"))
+                return tweet
             }
-            return tweets
+
+            fun fromJsonArray(jsonArray: JSONArray): List<Tweet> {
+                val tweets = ArrayList<Tweet>()
+                for (i in 0 until jsonArray.length()) {
+                    tweets.add(fromJson(jsonArray.getJSONObject(i)))
+                }
+                return tweets
+            }
         }
     }
-}
